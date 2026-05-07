2a3a6ef8e34aff788966f03381195fb908a2a685
kenryhuang <kenryhuang@gmail.com>
2026-05-07 20:59:43 +0800

    chore: scaffold vue phaser project

---
+++ src/app/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="app-shell">
+    <section class="game-root">
+      <h1>Pixel Absurd Survivor</h1>
+      <button type="button" class="primary-action">Start Run</button>
+    </section>
+  </main>
+</template>
